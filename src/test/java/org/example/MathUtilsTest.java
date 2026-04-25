1bf5279379d68aab4bb49e41a54c71d68ea10418
giabao848 <bdinhvugia@gmail.com>
2026-04-25 13:25:13 +0700

    Them test

--- src/test/java/org/example/MathUtilsTest.java
+++ src/test/java/org/example/MathUtilsTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import static org.junit.jupiter.api.Assertions.assertThrows;
 public class MathUtilsTest {
     private MathUtils math;
     @BeforeEach
@@ -18,5 +18,9 @@ public class MathUtilsTest {
     @Test
     void test1(){
         assertEquals(1, math.divide(2, 2));
+    }
+    @Test
+    void test3(){
+        assertThrows(ArithmeticException.class, () -> math.divide(2, 0))
     }
 }
